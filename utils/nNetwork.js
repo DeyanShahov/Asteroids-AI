4d223efb8213829da2ef08ba038be205b52bde3a
redfoxbg <redfoxbg@gmail.com>
2024-06-17 13:44:49 +0300

    Reset option

--- utils/nNetwork.js
+++ utils/nNetwork.js
@@ -1,73 +1,78 @@
 import Constants from '../constants.js';
+import { readyNeuralNetwork } from '../data/readyToUseAi.js';
 import { NeuralNetwork } from '../engine/NeuralNetwork.js';
 import { distanceBetweenPoints } from './asteroid.js';
 import { angleToPoint } from './game.js';
 
+const useRedyAiAsParameter = false;
 let nNetwork = undefined;
 
 export async function prepereNeuralNetwork(ship) {
-    // check for network in data file
-    let loadNetwork = await loadNetworkFromDataFile();
-    if (loadNetwork != null && loadNetwork instanceof NeuralNetwork) nNetwork = loadNetwork;
-    // on missing or error in data file, check in localStorage
-    if (loadNetwork == null) {
-        const data = loadNetworkFromLoacalStorage();
-        if (data != null && data instanceof NeuralNetwork) nNetwork = data;
-    }
+    if (useRedyAiAsParameter) nNetwork = loadNetworkAsParameter();
+    else {
+        // check for network in data file
+        let loadNetwork = await loadNetworkFromDataFile();
+        if (loadNetwork != null && loadNetwork instanceof NeuralNetwork) nNetwork = loadNetwork;
+        // on missing or error in data file, check in localStorage
+        if (loadNetwork == null) {
+            const data = loadNetworkFromLoacalStorage();
+            if (data != null && data instanceof NeuralNetwork) nNetwork = data;
+        }
 
-    if (nNetwork == undefined && !(nNetwork instanceof NeuralNetwork)) neuralNetworkTrein(ship);
+        if (nNetwork == undefined && !(nNetwork instanceof NeuralNetwork)) neuralNetworkTrein(ship);
+    }
 }
 
 function neuralNetworkTrein(ship) {
-        console.log('Train new neural network ...');
-        nNetwork = new NeuralNetwork(Constants.NUM_INPUTS, Constants.NUM_HIDDEN, Constants.NUM_OUTPUTS);
+    console.log('Train new neural network ...');
+    nNetwork = new NeuralNetwork(Constants.NUM_INPUTS, Constants.NUM_HIDDEN, Constants.NUM_OUTPUTS);
 
-        let ax, ay, sa, sx, sy;
+    let ax, ay, sa, sx, sy;
 
-        for (let i = 0; i < Constants.NUM_SAMPLES; i++) {
-            // random asteroids location
-            ax = Math.random() * (Constants.SCREEN_WIDTH + Constants.ROID_SIZE) - Constants.ROID_SIZE / 2;
-            ay = Math.random() * (Constants.SCREEN_HEIGHT + Constants.ROID_SIZE) - Constants.ROID_SIZE / 2;
+    for (let i = 0; i < Constants.NUM_SAMPLES; i++) {
+        // random asteroids location
+        ax = Math.random() * (Constants.SCREEN_WIDTH + Constants.ROID_SIZE) - Constants.ROID_SIZE / 2;
+        ay = Math.random() * (Constants.SCREEN_HEIGHT + Constants.ROID_SIZE) - Constants.ROID_SIZE / 2;
 
-            // ship angle and position
-            sa = Math.random() * Math.PI * 2;
-            sx = ship.x;
-            sy = ship.y;
+        // ship angle and position
+        sa = Math.random() * Math.PI * 2;
+        sx = ship.x;
+        sy = ship.y;
 
-            // calculate the angle to the asteroid
-            let angle = angleToPoint(sx, sy, sa, ax, ay);
+        // calculate the angle to the asteroid
+        let angle = angleToPoint(sx, sy, sa, ax, ay);
 
-            // determinate the drirection to turn
-            let direction = angle > Math.PI ? Constants.OUTPUT_LEFT : Constants.OUTPUT_RIGHT;
+        // determinate the drirection to turn
+        let direction = angle > Math.PI ? Constants.OUTPUT_LEFT : Constants.OUTPUT_RIGHT;
 
-            // train the network
-            nNetwork.trein(normaliseInputToNetwork(ax, ay, angle, sa), [direction]);
+        // train the network
+        nNetwork.trein(normaliseInputToNetwork(ax, ay, angle, sa), [direction]);
 
 
-        }
-        // // test train the network with XOR logic
-        // for (let i = 0; i < Constants.NUM_SAMPLES; i++) {
-        //     // TEST XOR logic
-        //     // 0 0 = 0
-        //     // 0 1 = 1
-        //     // 1 0 = 1
-        //     // 1 1 = 0
+    }
+    // // test train the network with XOR logic
+    // for (let i = 0; i < Constants.NUM_SAMPLES; i++) {
+    //     // TEST XOR logic
+    //     // 0 0 = 0
+    //     // 0 1 = 1
+    //     // 1 0 = 1
+    //     // 1 1 = 0
 
-        //     let input0 = Math.round(Math.random()); // 0 or 1 
-        //     let input1 = Math.round(Math.random()); // 0 or 1
-        //     let output = input0 == input1 ? 0 : 1;
-        //     nNetwork.trein([input0, input1], [output]); 
-        // }
+    //     let input0 = Math.round(Math.random()); // 0 or 1 
+    //     let input1 = Math.round(Math.random()); // 0 or 1
+    //     let output = input0 == input1 ? 0 : 1;
+    //     nNetwork.trein([input0, input1], [output]); 
+    // }
 
-        // // test output
-        // console.log('0, 0 = ' + nNetwork.feedForward([0, 0]).data);
-        // console.log('0, 1 = ' + nNetwork.feedForward([0, 1]).data);
-        // console.log('1, 0 = ' + nNetwork.feedForward([1, 0]).data);
-        // console.log('1, 1 = ' + nNetwork.feedForward([1, 1]).data);
+    // // test output
+    // console.log('0, 0 = ' + nNetwork.feedForward([0, 0]).data);
+    // console.log('0, 1 = ' + nNetwork.feedForward([0, 1]).data);
+    // console.log('1, 0 = ' + nNetwork.feedForward([1, 0]).data);
+    // console.log('1, 1 = ' + nNetwork.feedForward([1, 1]).data);
 
-        // save trained network
-        console.log('Ready newly trained neural network!');
-        saveNetworkToLocalStorage(nNetwork);
+    // save trained network
+    console.log('Ready newly trained neural network!');
+    saveNetworkToLocalStorage(nNetwork);
 }
 
 export function normaliseInputToNetwork(roidX, roidY, roidAngle, shipA) {
@@ -124,17 +129,17 @@ function loadNetworkFromLoacalStorage() {
             console.log('Loaded network from localStorage');
             const parsedData = JSON.parse(data);
             return NeuralNetwork.fromJSON(parsedData);
-        } 
-        if (data == null)  console.log('Local Storage dont contain saved neural network!');
+        }
+        if (data == null) console.log('Local Storage dont contain saved neural network!');
     } catch (error) {
         console.error('Failed to load network from localStorage:', error);
         return null;
-    } 
+    }
 }
 
 async function loadNetworkFromDataFile() {
     try {
-        const response = await fetch('../ddata/readyToUseAi.json');
+        const response = await fetch('../data/readyToUseAi.json');
         if (!response.ok) throw new Error('Network respons was not ok');
         console.log('Loaded network from data file');
         const data = await response.json();
@@ -147,5 +152,13 @@ async function loadNetworkFromDataFile() {
 }
 
 
+function loadNetworkAsParameter() {
+    // const data = readyNeuralNetwork;
+    // console.log('Loaded network as param');
+    // const parsedData = JSON.parse(data);
+    // const parsedData2 = data.json();
+    // console.log('a');
+    // return NeuralNetwork.fromJSON(parsedData2);
+}
 
 
